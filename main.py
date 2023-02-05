86c7d5718617a0a5817cbc0ecf8d0e584bd09db6
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2023-02-04 23:46:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -124,20 +124,6 @@ item_entry.pack(side='left')
 quantity_entry = tk.Entry(frame3, width=20)
 quantity_entry.pack(side='left')
 
-priceboughtCAD_entry = tk.Entry(frame3, width=20)
-priceboughtCAD_entry.pack(side='left')
-
-pricesellingCAD_entry = tk.Entry(frame3, width=20)
-pricesellingCAD_entry.pack(side='left')
-
-Ran_entry = tk.Entry(frame3, width=20)
-Ran_entry.pack(side='left')
-
-ASIN_entry = tk.Entry(frame3, width=20)
-ASIN_entry.pack(side='left')
-
-UPC_entry = tk.Entry(frame3, width=20)
-UPC_entry.pack(side='left')
 
 root.mainloop()
 

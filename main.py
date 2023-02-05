1499fd6902ba32c7eed0a41b504a0b0e354614bc
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2023-02-05 17:19:08 -0500

    almost done

--- main.py
+++ main.py
@@ -151,13 +151,14 @@ def show_add_screen():
     
 
 root = tk.Tk()
-root.geometry("700x700") # Set the width to 500 and height to 500
+root.config(bg='orange')
+root.geometry("700x550") # Set the width to 500 and height to 500
 root.title("Database App")
 
 
 
 frame3 = tk.Frame(root)
-frame3.pack(anchor='nw')
+frame3.pack( anchor='nw')
 
 image = Image.open("amazon.jpg")
 image.thumbnail((100,100))
@@ -170,13 +171,17 @@ photo = ImageTk.PhotoImage(image)
 label = tk.Label(frame3, image=photo)
 label.pack(side='left', padx=0)
 
-seller_label = tk.Label(frame3, text="ndrewid", font=("Helvetica", 20), bg="#ffffff", fg='orange')
+seller_label = tk.Label(frame3, text="ndrewid", font=("Helvetica", 40), bg="#ffffff", fg='orange')
 seller_label.pack(side='left')
 
-frame2 = tk.Frame(root)
-frame2.pack(side='left', anchor='nw')
+seller_label = tk.Label(frame3, text=" - if it does not exist, we are working one it.     ", font=("Helvetica", 15), bg="#ffffff", fg='red')
+seller_label.pack(side='left')
+
+frame2 = tk.Frame(root, bg="#FFA500")
+frame2.pack()
 
 seller_label = tk.Label(frame2, text="Seller:", font=("Helvetica", 10), bg="#ffffff")
+seller_label.config(background='orange')
 seller_label.pack(side='left')
 entry = tk.Entry(frame2, width=90)
 entry.pack(side='left')
@@ -185,20 +190,81 @@ entry.pack(side='left')
 add_item_button = tk.Button(frame2, text="Show seller Itmes", command=lambda: show_specific_database(entry.get()))
 add_item_button.pack(side='left', anchor='center')
 
-frame1 = tk.Frame(root)
-frame1.pack(anchor='nw')
+frame1 = tk.Frame(root, bg="#FFA500")
+frame1.pack()
 
-text_area = tk.Text(frame1, height=100, width=50)
+text_area = tk.Text(frame1, height=100, width=100)
 show_database_button = tk.Button(frame1, text="Show Database", command=show_database)
 show_database_button.pack(side='right', padx=20)
 
-frame4 = tk.Frame(root)
-frame4.pack()
-
-text_area2 = tk.Text(frame4, height=100, width=50)
-show_database_button = tk.Button(frame4, text="Add item", command=show_add_screen)
+text_area = tk.Text(frame1, height=100, width=100)
+show_database_button = tk.Button(frame1, text="Edit item", command=show_add_screen)
 show_database_button.pack(side='right', padx=20)
 
+frame4 = tk.Frame(root, bg="#FFA500")
+frame4.pack(anchor='nw')
+seller_label = tk.Label(frame4, text="\n\nAdd an Item")
+seller_label.config(background='orange')
+seller_label.pack(side='top')      
+# Entry for seller
+seller_label = tk.Label(frame4, text="Seller:")
+seller_label.config(background='orange')
+seller_label.pack(side='top')
+seller_entry = tk.Entry(frame4, width=120)
+seller_entry.pack(side='top')
+
+ # Entry for item
+item_label = tk.Label(frame4, text="Item:")
+item_label.config(background='orange')
+item_label.pack(side='top')
+item_entry = tk.Entry(frame4, width=120)
+item_entry.pack(side='top')
+
+# Entry for quantity
+quantity_label = tk.Label(frame4, text="Quantity:")
+quantity_label.config(background='orange')
+quantity_label.pack(side='top')
+quantity_entry = tk.Entry(frame4, width=120)
+quantity_entry.pack(side='top')
+
+# Entry for priceboughtCAD
+priceboughtCAD_label = tk.Label(frame4, text="Price Bought (CAD):")
+priceboughtCAD_label.config(background='orange')
+priceboughtCAD_label.pack(side='top')
+priceboughtCAD_entry = tk.Entry(frame4, width=120)
+priceboughtCAD_entry.pack(side='top')
+
+# Entry for pricesellingCAD
+pricesellingCAD_label = tk.Label(frame4, text="Price Selling (CAD):")
+pricesellingCAD_label.config(background='orange')
+pricesellingCAD_label.pack(side='top')
+pricesellingCAD_entry = tk.Entry(frame4, width=120)
+pricesellingCAD_entry.pack(side='top')
+
+# Entry for Ran
+Ran_label = tk.Label(frame4, text="Rank:")
+Ran_label.config(background='orange')
+Ran_label.pack(side='top')
+Ran_entry = tk.Entry(frame4, width=120)
+Ran_entry.pack(side='top')
+
+# Entry for ASIN
+ASIN_label = tk.Label(frame4, text="ASIN:")
+ASIN_label.config(background='orange')
+ASIN_label.pack(side='top')
+ASIN_entry = tk.Entry(frame4, width=120)
+ASIN_entry.pack(side='top')
+
+# Entry for UPS
+UPS_label = tk.Label(frame4, text="UPS:")
+UPS_label.config(background='orange')
+UPS_label.pack(side='top')
+UPS_entry = tk.Entry(frame4, width=120)
+UPS_entry.pack(side='top')
+
+# Add item button
+add_item_button = tk.Button(frame4, text="Add Item")
+add_item_button.pack(side='top', anchor='center')
 
 
 
